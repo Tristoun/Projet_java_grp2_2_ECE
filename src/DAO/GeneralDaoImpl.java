a4f128bbe161277757d2c564a3e0d079fd6a9517
matthieurob <matthieu.robert@edu.ece.fr>
2025-04-29 19:23:10 +0200

    setbyid devrait marcher

--- src/DAO/GeneralDaoImpl.java
+++ src/DAO/GeneralDaoImpl.java
@@ -24,11 +24,11 @@ public class GeneralDaoImpl {
             Statement statement = connexion.createStatement();
 
             // récupération des produits de la base de données avec la requete SELECT
-            res = statement.executeQuery("select * from " + this.table);        
-            if(res == null) {
+            res = statement.executeQuery("select * from " + this.table);
+            if (res == null) {
                 System.out.println("No values");
-            }    
-        }catch(SQLException e) {
+            }
+        } catch (SQLException e) {
             e.printStackTrace();
         }
 
@@ -37,28 +37,29 @@ public class GeneralDaoImpl {
 
     /**
      * Insérer un dictionnaire dans la bdd, Map est un type permettant de créer un "dictionnaire" comme en python
+     *
      * @params : On passe en type de Map le type de "key" et le type de la valeur
      * Le type Object représente toutes les types on peut donc mettre String, int, Date() ....
      */
-    public void insert(Map<String, Object> dictionnary) { 
+    public void insert(Map<String, Object> dictionnary) {
         StringBuilder columns = new StringBuilder();
         StringBuilder values = new StringBuilder();
 
         Set<String> keys = dictionnary.keySet();
         int index = 0;
 
-        for(String keysColumn : keys) {
+        for (String keysColumn : keys) {
             columns.append(keysColumn);
             values.append("?");
 
-            if(index < keys.size()-1) {
+            if (index < keys.size() - 1) {
                 columns.append(", ");
                 values.append(", ");
                 index++;
             }
-        } 
+        }
 
-        String query = "INSERT INTO "+this.table+"("+columns.toString()+") VALUES ("+values.toString()+")";
+        String query = "INSERT INTO " + this.table + "(" + columns.toString() + ") VALUES (" + values.toString() + ")";
         System.out.println(query); //Affiche le résultat de la query  !!! A RETIRER
 
         try {
@@ -66,54 +67,89 @@ public class GeneralDaoImpl {
             PreparedStatement statement = connexion.prepareStatement(query);
 
             int pos = 1;
-            for(String keyString : keys) {
+            for (String keyString : keys) {
                 statement.setObject(pos, dictionnary.get(keyString)); //On utilise setObject car on se sait pas qu'elle type de valeur on rentre, on s'adapte
                 pos++;
             }
 
             int res = statement.executeUpdate();
-            if(res != 1) {
+            if (res != 1) {
                 System.out.println("Error while insert values");
             }
-        } catch(SQLException e) {
+        } catch (SQLException e) {
             e.getStackTrace();
         }
     }
 
     /**
-     * Delete by id a column 
+     * Delete by id a column
      * Warning : Caution with foreign key in case of link
-     * @param columnName nom de la colonne 
-     * @param value valeur 
+     *
+     * @param columnName nom de la colonne
+     * @param value      valeur
      */
     public void delete(String columnName, int value) {
         try {
             Connection connextion = this.dao.getConnection();
-            String query = "DELETE FROM "+this.table+"WHERE "+columnName+"=?";
+            String query = "DELETE FROM " + this.table + "WHERE " + columnName + "=?";
             PreparedStatement statement = connextion.prepareStatement(query);
             statement.setInt(1, value);
             statement.executeUpdate();
 
-        } catch(SQLException e) {
+        } catch (SQLException e) {
             e.getStackTrace();
         }
     }
 
     /**
      * Set an element in the table (must be change to enable multiple set with Map)
+     *
      * @param columnName name of the column
      * @param value
      */
-    public void setById(String idName, int idValue, String columnName, Object value) {
+
+    public void setByID(Map<String, Object> dictionnary, int idValue) {
+        StringBuilder requestString = new StringBuilder();
+
+        Set<String> keys = dictionnary.keySet();
+        int index = 0;
+
+        for (String keyColumn : keys) {
+            requestString.append(keyColumn);
+            requestString.append(" = ?");
+
+
+            if (index < keys.size() - 1) {
+                requestString.append(", ");
+            }
+            index++;
+        }
+
+
+        String query = "UPDATE " + this.table + " SET " + requestString.toString() + " WHERE id = ?";
+        System.out.println(query);
+
         try {
             Connection connexion = this.dao.getConnection();
-            String query = "UPDATE "+this.table+" SET "+columnName+"=? WHERE "+idName+"=?";
             PreparedStatement statement = connexion.prepareStatement(query);
-            statement.setObject(1, value);
-            statement.setInt(2, idValue);
-            statement.executeUpdate();
-        } catch(SQLException e) {
-            e.getStackTrace();
+
+
+            int pos = 1;
+            for (String keyString : keys) {
+                statement.setObject(pos, dictionnary.get(keyString));
+                pos++;
+            }
+
+
+            statement.setObject(pos, idValue);
+
+
+            int res = statement.executeUpdate();
+            if (res != 1) {
+                System.out.println("Error while updating values");
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
         }
     }
 }
