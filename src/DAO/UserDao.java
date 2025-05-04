c859b783b703a829a8859805a82c5d7ccd1c7784
Vianney de Maximy <vianney.demaximy@edu.ece.fr>
2025-05-04 12:47:58 +0200

    On passe de DAO a Dao et ca change tout

--- src/DAO/UserDao.java
+++
@@ -1,7 +0,0 @@
-package DAO;
-
-public interface UserDao {
-    public void returnProfilPatient(int id_patient);
-    public void returnAllProfiles();
-    public void editProfileUser(int id_patient, String newName);
-}
