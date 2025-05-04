87ca040c03bc7ef0f5971db5402e13f56c3ea06a
Vianney de Maximy <vianney.demaximy@edu.ece.fr>
2025-05-04 10:43:34 +0200

    Normalement la c bon

--- src/main/java/DAO/SpecialistDao.java
+++ src/main/java/DAO/SpecialistDao.java
@@ -2,4 +2,7 @@ package DAO;
 
 
 public interface SpecialistDAO {
+    public void returnProfilPatient(int id_patient);
+    public void returnAllProfiles();
+    public void editProfileUser(int id_patient, String newName);
 }
