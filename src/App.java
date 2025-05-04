29f03fdb8d3642454bf9586e5dfd05646a593abe
antoinejlorsoni@gmail.com <you@example.com>
2025-05-04 16:15:44 +0200

    debut css boutons

--- src/App.java
+++ src/App.java
@@ -14,7 +14,7 @@ public class App extends Application {
     @Override
     public void start(Stage stage) throws Exception {
         DaoFactory.init("info_doctolib", "root", "patapouf");
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("/SceneDesign/login.fxml"));
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/SceneDesign/template.fxml"));
         AnchorPane root = loader.load();
         Scene scene = new Scene(root); // Set scene size and background color
 
