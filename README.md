2a60ff5d12ce649b8dc8cbfb433d4cdd89bc2fcd
Tristoun <tristan@tristanjean.com>
2025-05-11 22:55:39 +0200

    Update Readme file and fix multiple main file

--- README.md
+++ README.md
@@ -1,1 +1,45 @@
 # Projet_java_grp2_2_ECE
+
+## Run the project with VsCode
+
+### 1 - Create .vscode folder
+
+launch.json
+```json
+{
+    "version": "0.2.0",
+    "configurations": [
+      {
+        "type": "java",
+        "name": "Run JavaFX App",
+        "request": "launch",
+        "mainClass": "Main",
+        "vmArgs": "--module-path lib --add-modules javafx.controls,javafx.fxml"
+      }
+    ]
+  }
+```
+
+settings.json
+```json
+{
+    "java.project.sourcePaths": [
+      "src"
+    ],
+    "java.project.outputPath": "bin",
+    "java.project.referencedLibraries": [
+      "lib/**/*.jar"
+    ]
+  }
+```
+### 2- Create lib folder
+
+* Download MYSQL Connector https://dev.mysql.com/downloads/connector/j/
+* Download JavaFX https://gluonhq.com/products/javafx/
+* Put all the .jar in the lib folder
+
+### 3- Create Database
+
+* Go to PhpMyAdmin and create the database with the sql File **info_doctolib.sql**
+
+### 4- Install Java extensions
--- src/Main.java
+++ src/Main.java
@@ -1,40 +1,33 @@
-import java.sql.SQLException;
-import java.util.HashMap;
-import java.util.Map;
-
+import Application.Controller;
 import DAO.DaoFactory;
-import DAO.GeneralDaoImpl;
-import DAO.UserDaoImpl;
-import DAO.RDVDaoImpl;
-import java.sql.ResultSet;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
 
 
-import Models.RDV;
-import Models.Specialist;
-import Models.User;
-import Vue.*;
+public class Main extends Application {
+    public static void main(String[] args) throws Exception {
+        launch(args);
+    }
 
+    @Override
+    public void start(Stage stage) throws Exception {
+        System.out.println("start() method is being called.");
+        if (stage == null) {
+            throw new IllegalStateException("Stage is null in start method.");
+        }
 
-public class Main {
-    public static void main(String args[]) {
-        DaoFactory.init("info_doctolib", "root", "patapouf");
-        /*UserDaoImpl userDao = new UserDaoImpl();
-
-        ResultSet res = userDao.getAll();
-        GeneralVue.showOutput(res);
-        User user = new User(2,"Bernard","1234",0);
-        Specialist specialiste = new Specialist(3,"Dr Michmich","1234","Sympatoche",15.2,3.5);
-        RDV rdv = new RDV(1,2,3,null,2,"pas ouf"); //Le rating on l ajoutera apres
-
-
-        userDao.setById("idUser", 2, "name", "bob2");
-        userDao.delete("idUser", 1);
-
-        try {
-            DaoFactory.getConnection().close(); //Close the connection if used
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }*/
-
+        DaoFactory.init("info_doctolib", "root", "");
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/SceneDesign/login.fxml"));
+        AnchorPane root = loader.load();
+        Scene scene = new Scene(root); // Set scene size and background color
+        //String css = this.getClass().getResource("application.css").toExternalForm();
+        //scene.getStylesheets().add(css);
+        
+        stage.setScene(scene);
+        stage.setResizable(false);
+        stage.show();
     }
 }
